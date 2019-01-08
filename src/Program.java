33fdbab173483e5f27a3007e50be7e59de564d80
Nate Clark <natec425@gmail.com>
2019-01-08 12:43:50 -0600

    Start Program
    It currently collects input for a new
    JournalEntry but doesn't do anything
    with it.

---
+++ src/Program.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+            
+public class Program {
+    private static Scanner stdin = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        JournalEntry entry = inputJournalEntry();
+    }
+
+    private static JournalEntry inputJournalEntry() {
+        var author = inputAuthor();
+        var title = inputTitle();
+        var body = inputBody();
+        return new JournalEntry(author, new Date(), body, title);
+    }
+
+    private static String inputTitle() {
+        System.out.print("Title: ");
+        return stdin.nextLine().trim();
+    }
+
+    private static String inputBody() {
+        System.out.println("Body: (enter END on a line by itself to quit)");
+        var lines = new ArrayList<String>();
+        for (var line = stdin.nextLine(); !line.equals("END"); line = stdin.nextLine()) {
+            lines.add(line);
+        }
+        return String.join("\n", lines);
+    }
+
+    private static String inputAuthor() {
+        System.out.print("Author: ");
+        return stdin.nextLine().trim();
+    }
+}
